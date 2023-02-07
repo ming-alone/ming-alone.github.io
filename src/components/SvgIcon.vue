d7876f9a5494043f56a75c226c90a3a50336c684
tears <596231290@qq.com>
2023-02-07 10:35:05 +0800

    feat: 添加svg图标配置

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName" :fill="color" />
+  </svg>
+</template>
+
+<script lang="js">
+import {computed, defineComponent} from 'vue'
+export default defineComponent({
+  name: "SvgIcon",
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+  },
+  setup(props) {
+    return {
+      iconName: computed(() => `#icon-${props.iconClass}`),
+      svgClass: computed(() => {
+        if (props.className) {
+          return `svg-icon ${props.className}`
+        }
+        return 'svg-icon'
+      })
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.svg-icon {
+  width: 40px;
+  height: 40px;
+  position: relative;
+  fill: currentColor;
+  vertical-align: -2px;
+}
+</style>
